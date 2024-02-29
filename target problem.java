babe65dd9f910653ee4c3f3806cbbf069a50be10
yashwanth <121254050+yashwanth17230@users.noreply.github.com>
2024-02-29 14:42:02 +0530

    target problem.java

--- target problem.java
+++ target problem.java
@@ -30,3 +30,52 @@ public class Main
 	    }
 	}
 }
+
+
+/*import java.util.Scanner;
+import java.io.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    
+	    int n,i,t,j;
+	    int x[]=new int[10];
+	    Scanner sc= new Scanner(System.in);
+	    System.out.println("Enter the no of elements");
+	    n=sc.nextInt();
+	    for(i=0;i<n;i++){
+	         x[i]=sc.nextInt();
+	    }
+	    for( i=0;i<n;i++){
+	         System.out.println(x[i]);
+	    }
+	    System.out.println("Enter the target elements");
+	    t=sc.nextInt();
+	    int y[]=new int[10];
+	    y=sumOfTwo(x,t);
+	    for(int i=0;i<2;i++){
+	        System.out.println(y[i]);
+	    }
+	    Main int[] sumOfTwo(int x[],int t)
+	    {
+	        int z[]=new int[2];
+	        for(i=0;i<n;i++)
+	        {
+	            for(j=1;j<n;j++)
+	            {
+	                if(x[i]+x[j]==t)
+	            {
+	                z[0]=i;
+	                z[1]=j;
+	            }
+	            
+	        }
+	        
+	    }
+	    return z;
+	    }
+	    
+	}
+}
+
+*/
